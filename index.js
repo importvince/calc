4c40637cc73f5112b46434ac7f0314f10d91bc52
Vinny 2 Thumbs <97123672+importvince@users.noreply.github.com>
2023-08-30 16:23:21 -0500

    add display functionality. always add defer tag in script lol

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+//grab all numbers
+// const one = document.getElementById('one');
+// const two = document.getElementById('two');
+// const three = document.getElementById('three');
+// const four = document.getElementById('four');
+// const five = document.getElementById('five');
+// const six = document.getElementById('six');
+// const seven = document.getElementById('seven');
+// const eight = document.getElementById('eight');
+// const nine = document.getElementById('nine');
+// const zero = document.getElementById('ten');
+
+const numberBtns = document.querySelectorAll('.number')
+
+//grab all operators
+const addBtn = document.getElementById('+');
+const subtractBtn = document.getElementById('-');
+const multiplyBtn = document.getElementById('x');
+const divideBtn = document.getElementById('/');
+
+//grab all other buttons
+const reset = document.getElementById('reset');
+const backspace = document.getElementById('delete');
+const equals = document.getElementById('=');
+const period = document.getElementById('.');
+
+//grab display
+const display = document.getElementById('results');
+
+//display functions
+for (let i = 0; i < numberBtns.length; i++) {
+    numberBtns[i].addEventListener('click', function() {
+      display.innerHTML = numberBtns[i].innerHTML;
+    });
+}
+
+
 // basic functionality
 function add(a,b) {
     return a+b;
@@ -17,7 +54,7 @@ function divide(a,b) {
 
 function operate(a,b, operator) {
     if(operator == 'add') {
-        const added = add(a, b);
+        const add = added(a, b);
         console.log(added);
     } else if(operator == 'subtract') {
         const subtracted = subtract(a,b);
